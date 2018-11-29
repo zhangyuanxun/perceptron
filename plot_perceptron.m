e31bd1789dd67650988ca7b2021cfcafca4a21dd
Yuanxun Zhang <bigfishinriver@gmail.com>
2018-11-29 13:57:45 -0600

    modify code and demo

--- plot_perceptron.m
+++ plot_perceptron.m
@@ -56,9 +56,9 @@ frame = getframe(f);
 im = frame2im(frame); 
 [imind,cm] = rgb2ind(im,256); 
 if iter == 0
-    imwrite(imind,cm,'demo/demo3.gif', 'gif', 'Loopcount',inf); 
+    imwrite(imind,cm,'demo/demo4.gif', 'gif', 'Loopcount',inf); 
 else 
-    imwrite(imind,cm,'demo/demo3.gif', 'gif', 'WriteMode','append'); 
+    imwrite(imind,cm,'demo/demo4.gif', 'gif', 'WriteMode','append'); 
 end 
 
 end
